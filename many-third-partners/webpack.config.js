b820f58e9d995c680404a3d29b6ed88b1708a23b
lekzinn <albsilva@outlook.com>
2020-05-27 22:27:27 -0300

    example many-third-partners

---
+++ many-third-partners/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new BundleAnalyzerPlugin()
+    ],
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+};
